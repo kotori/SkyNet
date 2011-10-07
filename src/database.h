9ee161ba5a2e49f56c54dddf83333009f53c8d0e
Kotori <kotori@greenskin.hopto.org>
2011-10-07 03:26:47 -0500

    File Structure Update.

---
+++ src/database.h
@@ -0,0 +1,35 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+// Default location of the database
+#define DEFAULT_LOC "database.db"
+
+// Turn off/on logging
+#define ACTIVE_LOGGING false
+
+namespace KotoriApp
+{
+	class Database
+	{
+	public:
+		Database();
+		Database(char *loc);
+		~Database();
+
+		bool SelectInt(int &refVal, const char *sql, ...);
+		char* Format(const char *msg, ...);
+	private:
+		void Common();
+		void Close();
+		bool Open();
+
+		bool is_open_;
+		bool log_enabled_;
+		char *db_loc_;
+		sqlite3 *db_;
+		sqlite3_stmt *stmt_;
+	};
+}
+
+#endif // DATABASE_H
+
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+// Common Includes.
+#include <cstdlib>
+#include <iostream>
+
+// Boost Libraries.
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+
+// Network Server Library.
+#include "network_server.hpp"
+
+// Boost ASIO Namespace.
+using boost::asio::ip::tcp;
+
+// Application's entry point.
+int main(int argc, char* argv[])
+{
+	try
+	{
+		// Ensure an argument has been passed, otherwise show usage.
+		if(argc != 2)
+		{
+			std::cerr << "Usage: " << argv[0] << " <port>\n";
+			return 1;
+		}
+		else
+		{
+			// Create a boost::asio io_service object.
+			boost::asio::io_service io_service;
+			KotoriApp::Server s(io_service, atoi(argv[1]));
+			io_service.run();
+		}
+	}
+	catch(std::exception& excp)
+	{
+		// If an exception has been caught, display it then exit.
+		std::cerr << "Exception: " << excp.what() << "\n";
+		return 1;
+	}
+
+	return 0;
+}
